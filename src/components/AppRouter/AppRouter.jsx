93a5986301076f7ffc21afb28db65b917b879625
Ruslan Mirets <ruslan.mirets@gmail.com>
2021-12-27 10:25:15 +0300

    Mobx, глобальный стейт, UserStore

--- src/components/AppRouter/AppRouter.jsx
+++ src/components/AppRouter/AppRouter.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
+import { Context } from '../..';
 import { adminRoutes, publicRoutes } from '../../routes';
 import { HOME_ROUTE } from '../../utils/consts';
 import styles from './AppRouter.module.scss';
 
 const AppRouter = () => {
-  const isAuth = false;
+  const { user } = useContext(Context);
+
   return (
     <main className={styles.main}>
       <div className="container">
         <Routes>
-          {isAuth &&
+          {user.isAuth &&
             adminRoutes.map(({ path, Component }) => (
               <Route key={path} path={path} element={Component} exact />
             ))}
--- src/components/HeaderAuth/HeaderAuth.jsx
+++ src/components/HeaderAuth/HeaderAuth.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { Context } from '../..';
 import styles from './HeaderAuth.module.scss';
 
 const HeaderAuth = () => {
+  const { user } = useContext(Context);
+
   return (
-    <Link className={styles.login} to='/login'>
-      <img src="/static/images/login.png" alt="Login" />
-    </Link>
+    <div className={styles.user}>
+      {user.isAuth ? (
+        <div className={styles.logout}>Выйти</div>
+      ) : (
+        <Link className={styles.login} to="/login">
+          <img src="/static/images/login.png" alt="Login" />
+        </Link>
+      )}
+    </div>
   );
 };
 
